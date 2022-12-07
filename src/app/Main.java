bf4f8f528dfe776553300e086e6dbaac7a47f164
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2022-12-07 20:20:09 +0200

    Removal of variables

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,19 +1,29 @@
 package app;
 
+
 public class Main {
+    static int a;
+    static int b;
+    static int c;
+    static int f;
+    static int d;
+    static int g;
 
     public static void main(String[] args) {
         System.out.println("My app for Users!");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
+        a = 8;
+        b = 2;
+        c = add(a, b);
         System.out.println(c);
-        int d = multiply(a, b);
+
+        d = multiply(a, b);
         System.out.println(d);
-        int f = subtraction(a, b);
+
+        f = subtraction(a, b);
         System.out.println(f);
-        int g = division(a, b);
+
+        g = division(a, b);
         System.out.println(g);
     }
 
